6d4420d397df80269444261dc11b9305e138d52c
uvaaland <ubvaaland@gmail.com>
2019-01-11 13:06:52 -0500

    Add shuffle feature

--- demo.py
+++ demo.py
@@ -1,8 +1,11 @@
 import glob
+from random import shuffle
 
 
 def get_images():
-    return glob.glob("gallery/*.png")
+    images = glob.glob("gallery/*.png")
+    shuffle(images)
+    return images
 
 
 def format_html(images):
@@ -11,18 +14,16 @@ def format_html(images):
 
     stylesheet = "<link rel=\"stylesheet\" href=\"css/theme/night.css\">\n"
     title = "<section><h1>Agents</h1></section>\n"
-    img = "<section><img src=\"../{}\"></section>\n"
+    img = "<section><img src=\"../{}\" width=\"1200\" height=\"500\"></section>\n"
 
     content = []
     for line in lines:
         if "<link rel=\"stylesheet\" href=\"css/theme/black.css\">" in line:
             content.append(" "*4 + stylesheet)
-        elif "<div class=\"slides\">" in line:
-            content.append(line)
+        elif "<section>Slide 1</section>" in line:
             content.append(" "*8 + title)
-            for image in images[:2]:
+            for image in images:
                 content.append(" "*8 + img.format(image))
-            next(lines) # skip <section>Slide 1</section>
             next(lines) # skip <section>Slide 2</section>
         else:
             content.append(line)
